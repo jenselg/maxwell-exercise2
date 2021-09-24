4f3bac1d7c744cbf5278bc0a49c72d2f74988101
Jensel Gatchalian <jgatchalian@bti-usa.com>
2021-09-23 20:11:03 -0700

    change argument names; create calculate_discounts method; raise error for when discounts.json and prices.json cannot be read;

--- price_calculator.rb
+++ price_calculator.rb
@@ -21,15 +21,14 @@ module PriceCalculator
     end # self.init ...
 
     # get output from init method and calculate prices based on quantity
-    def self.calculate_prices(output_init)
+    def self.calculate_prices(products)
 
-        user_products = output_init
         product_price_array = []
         prices = JSON.parse(File.open('prices.json').read) rescue nil
         
         unless prices.nil?
 
-            user_products.each do |product|
+            products.each do |product|
 
                 curr_product_index = product_price_array.find_index { |product_price| product_price["name"].downcase == product.downcase }
                 curr_price_index = prices.find_index { |price| price["name"].downcase == product.downcase }
@@ -48,21 +47,59 @@ module PriceCalculator
 
             end
 
-            calculate_discounts(product_price_array)
+            if product_price_array.size > 0
+                calculate_discounts(product_price_array)
+            else
+                puts "The item(s) you entered do not have a price defined."
+            end
 
         else
-            puts "Could not read and parse prices.json!"
+            raise "Could not read and parse prices.json!"
         end
 
     end # self.calculate_prices ...
 
     # get output from calculate_prices method and calculate any applicable discounts
-    def self.calculate_discounts(output_calculate_prices)
-        puts output_calculate_prices
+    def self.calculate_discounts(products)
+
+        discounts = JSON.parse(File.open('discounts.json').read) rescue nil
+
+        unless discounts.nil?
+
+            products.each do |product|
+
+                product["savings"] = 0.00
+                product_discount_index = discounts.find_index { |discount| discount["product"].downcase == product["name"].downcase }
+                
+                unless product_discount_index.nil?
+
+                    discount = discounts[product_discount_index]
+                    if product["quantity"].to_i >= discount["quantity"].to_i
+
+                        discount_multiplier = product["quantity"].to_i / discount["quantity"].to_i
+                        discount_quantity_remainder = product["quantity"].to_i % discount["quantity"].to_i
+                        total_no_discount = product["total"].to_f
+                        total_with_discount = (discount_multiplier * discount["price"].to_f) + (product["price"].to_f * discount_quantity_remainder)
+                        product["total"] = total_with_discount
+                        product["savings"] = (total_no_discount - total_with_discount).round(2)
+
+                    end
+
+                end
+
+            end
+
+            build_output(products)
+
+        else
+            raise "Could not read and parse discounts.json!"
+        end
+
     end # self.calculate_discounts ...
 
     # get output from calculate_discounts method and build CLI table and other details
-    def self.build_output(output_calculate_discounts)
+    def self.build_output(products)
+        puts products
     end # self.build_output ...
 
 end # module PriceCalculator ...
